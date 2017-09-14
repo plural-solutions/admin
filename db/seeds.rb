f58f1c8d9349c5b52a77e6545df15ec81f0a0da9
lucasgomide <lucaslg200@gmail.com>
2017-09-14 18:40:52 -0300

    Creating AdminUser on seed runs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Inserting AdminUsers.."
+User.create!(
+  email: 'admin@menyoo.com.br',
+  password: '123456',
+  role: 'admin'
+)
