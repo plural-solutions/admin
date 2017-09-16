933863b8b24517d16ef874eacd2c12df484d9bc4
lucasgomide <lucaslg200@gmail.com>
2017-09-16 19:02:22 -0300

    Updating default root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "rails_admin/main#dashboard"
 end
